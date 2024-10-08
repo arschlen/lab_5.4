75b6e3febc325a27fa5288231e7293a75b55e8eb
arschlen <arsnchld08@gmail.com>
2024-10-08 14:29:40 +0300

    arschlen

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5.4/5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double result = S0(1, 5);
+			Assert::AreEqual(result, 13.273068001, 0.0001);
+		}
+	};
+}
